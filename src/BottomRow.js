528ba5ae3b8830a74c24fa107abe8b375bf20ed1
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2019-11-13 22:09:37 -0700

    Pretty sure I reached mvp and stretch.

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,25 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const BottomRow = () => {
-  const [quarter, setQuarter] = useState(1);
-  const newQuart = () => {
-    if (quarter < 4 || quarter === 1) {
-      setQuarter(quarter + 1);
-    } else {
-      setQuarter(1);
-    }
-  };
+const BottomRow = props => {
   return (
     <div className="bottomRow">
-      <div>
-        <button className="quarter__button" onClick={newQuart}>
-          New Quarter
-        </button>
-      </div>
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{props.down}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
@@ -31,7 +18,7 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">{quarter}</div>
+        <div className="quarter__value">{props.quarter}</div>
       </div>
     </div>
   );
